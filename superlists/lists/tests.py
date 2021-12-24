9552482a300814fd6ba5ba07958727c64f8c6bc7
tsubasanut <tsubasanut@gmail.com>
2021-12-24 15:14:42 +0300

    test1

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
 class MyTest(TestCase):
 
-    def bad_math(self):
+    def test_bad_maths(self):
         self.assertEqual(1 + 1, 3)
