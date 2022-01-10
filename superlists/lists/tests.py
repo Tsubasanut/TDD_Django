f8664c18ff21f52c82657bff75e18a5c9c445beb
tsubasanut <tsubasanut@gmail.com>
2022-01-10 16:06:10 +0300

    chapter 5.1

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -24,3 +24,7 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         print((html  == expected_html))
         print(repr(html))'''
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/',  data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
