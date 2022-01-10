8be93f0b35f2f0d56cdda9a014fc41d6d7425dc0
tsubasanut <tsubasanut@gmail.com>
2022-01-10 14:45:16 +0300

    chapter 4.3

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common import keys
+from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+from pdb import set_trace
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -12,6 +13,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        set_trace()
+
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
@@ -27,19 +30,22 @@ class NewVisitorTest(unittest.TestCase):
                         'Enter a to-do item'
                         )
 
+
+
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            'No such line in table!'
         )
 
         # There is still a text box inviting her to add another item. She
