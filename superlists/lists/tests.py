22fc4fed7f6a263c57bdc214b3028dc9effcfbc1
tsubasanut <tsubasanut@gmail.com>
2022-01-10 14:01:43 +0300

    chapter 4.2

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -11,9 +12,15 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+        '''
+        # old code chapter 3
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        expected_html = render_to_string('home.html')
+        print((html  == expected_html))
+        print(repr(html))'''
