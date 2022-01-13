3828220430940d62f4ede3d2210de14d4ef543ef
tsubasanut <tsubasanut@gmail.com>
2022-01-13 11:44:28 +0300

    7.12

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,16 +6,14 @@ from django.template.loader import render_to_string
 from lists.models import Item, List
 
 
-
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    #def test_home_page(self):
+    # def test_home_page(self):
     #    found = resolve('/')
     #    self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
@@ -40,6 +38,7 @@ class HomePageTest(TestCase):
 
         self.assertIn('itemey1', response.content.decode())
         self.assertIn('itemey2', response.content.decode())
+
 
 class ListAndItemModelTest(TestCase):
 
@@ -70,27 +69,68 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(first_saved_item.list, saved_list)
         self.assertEqual(second_saved_item.list, saved_list)
 
+
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_1 = List.objects.create()
+        response = self.client.get(f'/lists/{list_1.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
+    def test_passes_correct_list_to_template(self):
+        w_list = List.objects.create()
+        list_1 = List.objects.create()
+        response = self.client.get(f'/lists/{list_1.id}/')
+        self.assertEqual(response.context['list'], list_1)
+
+    def test_displays_only_items_for_that_list(self):
         list_1 = List.objects.create()
         Item.objects.create(text='itemey1', list=list_1)
         Item.objects.create(text='itemey2', list=list_1)
+
+        list_2 = List.objects.create()
+        Item.objects.create(text='itemey3', list=list_2)
+        Item.objects.create(text='itemey4', list=list_2)
+
+        response = self.client.get(f'/lists/{list_1.id}/')
+
+        self.assertContains(response, 'itemey1')
+        self.assertContains(response, 'itemey2')
+        self.assertNotContains(response, 'itemey3')
+        self.assertNotContains(response, 'itemey4')
 
 
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/lists/new',  data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_can_redirect(self):
-        response = self.client.post('/lists/new',  data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, f'/lists/{List.objects.first().id}/')
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(f'/lists/{correct_list.id}/add_item',
+                         data={'item_text': 'A new item ot existing list'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item ot existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(f'/lists/{correct_list.id}/add_item',
+                                    data={'item_text': 'A new item ot existing list'})
+        self.assertRedirects(response,f'/lists/{correct_list.id}/')
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -7,12 +7,17 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
-
+def view_list(request, list_id):
+    filter_list = List.objects.get(id=list_id)
+    #items = Item.objects.filter(list=filter_list)
+    return render(request, 'list.html', {'list': filter_list})
 
 def new_list(request):
     list_1 = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_1)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_1.id}/')
+
+def add_item(request, list_id):
+    list_1 = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_1)
+    return redirect(f'/lists/{list_1.id}/')
