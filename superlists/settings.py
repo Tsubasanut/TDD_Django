b6acbc9728fd613bc5e66f0750fd7623b67124ac
tsubasanut <tsubasanut@gmail.com>
2022-01-25 13:42:41 +0300

    use env vars for prod settings DEBUG, ALLOWED_HOSTS, SECRET_KEY

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+'''
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 's)yn#ydi)o&fle1(-hx(d9i+s+6l+!-x*4h+^!4@)^#)yag4sj'
 
@@ -26,7 +27,15 @@ SECRET_KEY = 's)yn#ydi)o&fle1(-hx(d9i+s+6l+!-x*4h+^!4@)^#)yag4sj'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+'''
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -118,5 +127,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
