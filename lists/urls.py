3790d0322f6d7c1149711fe0d04f1fc8f69bbb09
tsubasanut <tsubasanut@gmail.com>
2022-02-10 12:49:04 +0300

    try to fix server side ch 14

--- lists/urls.py
+++ lists/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 #from django.contrib import admin
 from lists import views
 
+
 urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
