be477bec0c8ad482a1b47fa29b01a5ea5a202b72
tsubasanut <tsubasanut@gmail.com>
2022-02-11 12:42:39 +0300

    fix

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 #from django.contrib import admin
-from lists import views as lists_views
 from lists import urls as lists_urls
+from lists import views as lists_views
+
 
 urlpatterns = [
     url(r'^$', lists_views.home_page, name='home'),
