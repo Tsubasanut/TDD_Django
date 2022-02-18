3a0e1ebce578e33fab3cafe70d1f5cc4e2516a79
tsubasanut <tsubasanut@gmail.com>
2022-02-18 14:22:34 +0300

    18.3 Despiking

--- superlists/settings.py
+++ superlists/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -94,6 +95,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'accounts.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
